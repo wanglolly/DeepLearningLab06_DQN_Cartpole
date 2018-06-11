81115de5530c72e56e7e4fe133b0a696e363c913
wanglolly <wanglolly@gmail.com>
2018-06-12 02:03:53 +0800

    modify scripts

--- DQN_Cartpole.py
+++ DQN_Cartpole.py
@@ -165,7 +165,7 @@ def main():
                 state = env.reset()
                 state = torch.from_numpy(state.reshape((-1, 4))).float()
                 for j in range(STEP):
-                    env.render()
+                    #env.render()
                     action = dqn.action(state)
                     state,reward,done,_ = env.step(action[0,0])
                     state = torch.from_numpy(state.reshape((-1, 4))).float()
