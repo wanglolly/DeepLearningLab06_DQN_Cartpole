8896704b1a393a054d42940845aaee4a5b1f5677
wanglolly <wanglolly@gmail.com>
2018-06-15 14:38:32 +0800

    debug

--- DQN_Cartpole.py
+++ DQN_Cartpole.py
@@ -92,9 +92,9 @@ class DQN(nn.Module):
         if self.epsilon >= EPS_END:
             self.epsilon *= EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
         steps_done += 1
-        if random.random() > self.epsilon:
-            return self.action(state)
-        else:
+        #if random.random() > self.epsilon:
+        #    return self.action(state)
+        #else:
             return LongTensor([[random.randrange(self.action_dim)]])
         
     def action(self,state):
